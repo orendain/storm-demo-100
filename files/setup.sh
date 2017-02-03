570f643c61c22e56cf8134eec372967304857b59
Edgar Orendain <edgar@orendainx.com>
2017-02-02 19:26:59 -0800

    Compile latest jar for use

--- files/setup.sh
+++ files/setup.sh
@@ -3,7 +3,7 @@
 PROJ_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 DEST_DIR=/tmp/storm-tutorial
 
-# Move input files to accessible location
+# Move input files to accessible locations
 mkdir -p -m 777 $DEST_DIR
 cp $PROJ_DIR/src/main/resources/truck-input.txt $DEST_DIR
 cp $PROJ_DIR/src/main/resources/traffic-input.txt $DEST_DIR
