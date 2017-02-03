1cc0ea9a27cd44ae4ae5bfadcb98d15601052a6e
Edgar Orendain <edgar@orendainx.com>
2017-02-02 18:15:50 -0800

    Fixed setup.sh

--- files/setup.sh
+++ files/setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+PROJ_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 DEST_DIR=/tmp/tutorial/storm
 
 # Move input files to accessible location
 mkdir -p $DEST_DIR
-cp ../src/main/resources/truck-input.txt $DEST_DIR
-cp ../src/main/resources/traffic-input.txt $DEST_DIR
+cp $PROJ_DIR/src/main/resources/truck-input.txt $DEST_DIR
+cp $PROJ_DIR/src/main/resources/traffic-input.txt $DEST_DIR
